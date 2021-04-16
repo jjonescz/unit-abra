f2fe45483f0793c1e20c135e30bfa400d53962bc
Martin Vejbora <vejby@polygoniq.com>
2021-04-16 12:43:16 +0200

    Add mock message invalid login

--- src/routes/LoginScreen.svelte
+++ src/routes/LoginScreen.svelte
@@ -5,9 +5,12 @@
         ButtonSet,
         Tile,
         FluidForm,
+        InlineNotification,
         TextInput,
         PasswordInput,
     } from "carbon-components-svelte";
+
+    let invalid_login = false
   </script>
 
 <svelte:head><link rel="stylesheet" href="https://unpkg.com/carbon-components-svelte@0.30.0/css/g10.css" /></svelte:head>
@@ -15,22 +18,31 @@
 <div style="padding: 2rem;">
     <Tile>
         <FluidForm>
+            {#if invalid_login}
+                <InlineNotification
+                    lowContrast
+                    subtitle="Invalid login or password."
+                    on:close={() => invalid_login = false}
+                />
+            {/if}
             <TextInput
                 required
                 labelText="User name"
                 placeholder="Enter user name..."
+                
             />
             <PasswordInput
                 required
                 type="password"
                 labelText="Password"
-                placeholder="Enter password..."
+                placeholder="Enter password..."                
+                invalidText="Incorrect user name or password."
             />
         </FluidForm>
 
         <ButtonSet>
-            <Button kind="primary">
-                Login
+            <Button kind="primary" on:click={() => invalid_login = true}>
+                Log In
             </Button>
         </ButtonSet>
     </Tile>
