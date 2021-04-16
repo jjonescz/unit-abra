3d0e024a8d82ce84c289c1360b6d3085f75797bf
Jan Joneš <jjones@outlook.cz>
2021-04-16 20:14:50 +0200

    Fix `sensors` page

--- src/routes/sensors.svelte
+++ src/routes/sensors.svelte
@@ -1,29 +1,13 @@
 <script context="module">
-	import { browser } from '$app/env';
+	import { startChecking, lastCheck, lastError } from '$lib/server/sensors';
 
-	export async function load() {
-		if (!browser) {
-			const { startChecking, lastCheck, lastError } = await import('$lib/server/sensors');
+	export const hydrate = false;
 
-			startChecking();
-			return {
-				props: {
-					time: lastCheck,
-					error: lastError
-				}
-			};
-		}
-		return {};
-	}
-</script>
-
-<script>
-	export let time;
-	export let error;
+	startChecking();
 </script>
 
 <h1>Sensor checking</h1>
 
-<p>Last checked: {time}</p>
+<p>Last checked: {lastCheck}</p>
 
-<p>Last error: {error}</p>
+<p>Last error: {lastError}</p>
