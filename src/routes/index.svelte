06e5f4bc4902f0a9629479e1b87265c4f7396f24
Jan Joneš <jjones@outlook.cz>
2021-04-16 21:30:27 +0200

    Add UI shell

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,37 @@
 <script>
 	import Employee from '$lib/ui/Employee.svelte';
+	import {
+		Header,
+		HeaderAction,
+		HeaderPanelLink,
+		HeaderPanelLinks,
+		HeaderUtilities,
+		SkipToContent,
+		Content
+	} from 'carbon-components-svelte';
 	import LoginScreen from './LoginScreen.svelte';
 
 	let user;
 </script>
 
-<svelte:head>
-	<title />
-</svelte:head>
-
 {#if !user}
 	<LoginScreen bind:user />
 {:else}
-	<Employee bind:user />
+	<Header company="ABRA" platformName="Parking" href="/">
+		<div slot="skip-to-content">
+			<SkipToContent />
+		</div>
+
+		<HeaderUtilities>
+			<HeaderAction>
+				<HeaderPanelLinks>
+					<HeaderPanelLink>Log out</HeaderPanelLink>
+				</HeaderPanelLinks>
+			</HeaderAction>
+		</HeaderUtilities>
+	</Header>
+
+	<Content>
+		<Employee bind:user />
+	</Content>
 {/if}
