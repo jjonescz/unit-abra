edce7f6ca0bea9b3fd5d73f7d96862c62f953892
Jan Joneš <jjones@outlook.cz>
2021-04-17 00:27:18 +0200

    Improve tile UI

--- src/lib/ui/Employee.svelte
+++ src/lib/ui/Employee.svelte
@@ -151,24 +151,35 @@
 	kind="tertiary"
 	iconDescription="Refresh"
 	icon={WatsonHealthRotate_36016}
-	on:click={() => loadReservations()}
+	on:click={() => {
+		reservations.set(null);
+		loadReservations();
+	}}
 />
 
 {#if $reservations == null}
-	<SkeletonPlaceholder style="width: 100%; height: 2rem;" />
+	<SkeletonPlaceholder style="width: 100%; height: 5rem;" />
 {:else}
 	{#each $reservations as r (r.id)}
-		<Tile>
-			<div>
-				{r.slot}: {format(new Date(r.start), 'yyyy-MM-dd HH:mm')} for {r.duration} minutes
-			</div>
-			<div>
-				<Button
-					kind="danger-tertiary"
-					iconDescription="Delete"
-					icon={TrashCan16}
-					on:click={() => deleteReservation(r.id)}
-				/>
+		<Tile style="margin-bottom: 0.5rem;">
+			<div class="reservation">
+				<div class="slot">
+					{r.slot}
+				</div>
+				<div class="time">
+					{format(new Date(r.start), 'yyyy-MM-dd HH:mm')}
+				</div>
+				<div class="duration">
+					{r.duration} minute{r.duration === 1 ? '' : 's'}
+				</div>
+				<div class="delete">
+					<Button
+						kind="danger-tertiary"
+						iconDescription="Delete"
+						icon={TrashCan16}
+						on:click={() => deleteReservation(r.id)}
+					/>
+				</div>
 			</div>
 		</Tile>
 	{:else}
@@ -188,5 +199,57 @@
 	:global(.bx--date-picker > div) {
 		margin-top: 0.5rem;
 		margin-right: 0.125rem;
+	}
+
+	.reservation {
+		display: flex;
+		align-items: center;
+
+		@media (max-width: 640px) {
+			flex-wrap: wrap;
+		}
+
+		> div {
+			padding-right: 1rem;
+
+			@media (max-width: 640px) {
+				flex: 1 0 auto;
+			}
+		}
+
+		> .slot,
+		> .duration {
+			@media (max-width: 640px) {
+				width: 30%;
+			}
+		}
+
+		> .time,
+		> .delete {
+			@media (max-width: 640px) {
+				width: 70%;
+			}
+		}
+
+		> .slot {
+			font-size: 3rem;
+
+			@media (max-width: 640px) {
+				font-size: 2rem;
+			}
+		}
+
+		> .time {
+			font-size: 1.5rem;
+
+			@media (max-width: 640px) {
+				font-size: 1rem;
+			}
+		}
+
+		> .delete {
+			margin-left: auto;
+			text-align: right;
+		}
 	}
 </style>
