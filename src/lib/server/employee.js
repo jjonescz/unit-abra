ef6c0b8d6865a118715fdf8fcfeba0de61e03b60
Jan Joneš <jjones@outlook.cz>
2021-04-16 12:26:33 +0200

    Use dayjs to create reservations

--- src/lib/server/employee.js
+++ src/lib/server/employee.js
@@ -1,6 +1,3 @@
 import { writable } from "svelte/store";
 
-export const reservations = writable([
-    { start: new Date(2021, 4, 16, 15, 0), duration: 60, slot: 101 },
-    { start: new Date(2021, 4, 16, 20, 0), duration: 120, slot: 101 }
-]);
+export const reservations = writable([]);
--- src/lib/ui/Employee.svelte
+++ src/lib/ui/Employee.svelte
@@ -6,42 +6,65 @@
 		DatePicker,
 		DatePickerInput,
 		Form,
-		FluidForm,
 		Tile,
 		TimePicker,
 		DatePickerSkeleton
 	} from 'carbon-components-svelte';
 	import { TrashCan16 } from 'carbon-icons-svelte';
 	import dayjs from 'dayjs';
+	import customParseFormatPlugin from 'dayjs/plugin/customParseFormat';
+	import durationPlugin from 'dayjs/plugin/duration';
+	import relativeTimePlugin from 'dayjs/plugin/relativeTime';
+
+	dayjs.extend(customParseFormatPlugin);
+	dayjs.extend(durationPlugin);
+	dayjs.extend(relativeTimePlugin);
 
 	let now = dayjs(new Date());
 
 	// User inputs
-	let date = now.format('YYYY-MM-DD');
-	let time = now.format('hh:mm');
-	let duration = '1:00';
+	let dateInput = now.format('YYYY-MM-DD');
+	let timeInput = now.format('H:mm');
+	let durationInput = '1:00';
+
+	// Validation
+	$: date = dayjs(dateInput, 'YYYY-MM-DD', true);
+	$: time = dayjs(timeInput, ['HH:mm', 'H:mm'], true);
+	$: duration = dayjs(durationInput, ['HH:mm', 'H:mm'], true);
+
+	$: timeDur = toDuration(time);
+	$: durationDur = toDuration(duration);
+	$: fullDate = date.startOf('day').add(timeDur);
+
+	function toDuration(date) {
+		return dayjs.duration(date.diff(date.startOf('day')));
+	}
 </script>
 
 <h2>New reservation</h2>
 
 <Form>
 	{#if browser}
-		<DatePicker datePickerType="single" dateFormat="Y-m-d" minDate={now} bind:value={date}>
+		<DatePicker datePickerType="single" dateFormat="Y-m-d" minDate={now} bind:value={dateInput}>
 			<DatePickerInput labelText="Date" placeholder="yyyy-mm-dd" pattern=".*" />
 		</DatePicker>
 	{:else}
 		<DatePickerSkeleton />
 	{/if}
-	<TimePicker labelText="Time" bind:value={time} pattern=".*" />
-	<TimePicker labelText="Duration" bind:value={duration} pattern=".*" />
+	<TimePicker labelText="Time" bind:value={timeInput} pattern=".*" />
+	<TimePicker labelText="Duration" bind:value={durationInput} pattern=".*" />
+	<p>
+		Selected time {timeDur.humanize()}, duration {durationDur.humanize()}. Date: {fullDate.format(
+			'YYYY-MM-DD HH:mm'
+		)}.
+	</p>
 	<Button
 		type="submit"
 		on:click={() =>
 			reservations.update((r) => {
-				const s = `${new Date(date).toDateString()} ${time}`;
 				r.unshift({
-					start: new Date(s),
-					duration: 60,
+					start: fullDate,
+					duration: durationDur.asMinutes(),
 					slot: 100 + Math.round(Math.random() * 20)
 				});
 				return r;
@@ -56,7 +79,7 @@
 {#each $reservations as r, i}
 	<Tile>
 		<div>
-			{r.slot}: {r.start} for {r.duration} minutes
+			{r.slot}: {r.start.format('YYYY-MM-DD HH:mm')} for {r.duration} minutes
 		</div>
 		<div>
 			<Button
