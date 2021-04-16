d51fc4df9e6662d7060ee889034ca7146e87fb7a
Jan Joneš <jjones@outlook.cz>
2021-04-16 10:58:05 +0200

    Use reservations store

--- src/lib/server/employee.js
+++ src/lib/server/employee.js
@@ -1,16 +1,6 @@
-export class Reservation {
-    /** @type {Date} */
-    start;
-    /** @type {Number} in minutes */
-    duration;
-    /** @type {Number} parking slot */
-    slot;
-}
+import { writable } from "svelte/store";
 
-/** @returns {Reservation[]} */
-export function getReservations() {
-    return [
-        { start: new Date(2021, 4, 16, 15, 0), duration: 60, slot: 101 },
-        { start: new Date(2021, 4, 16, 20, 0), duration: 120, slot: 101 }
-    ];
-}
+export const reservations = writable([
+    { start: new Date(2021, 4, 16, 15, 0), duration: 60, slot: 101 },
+    { start: new Date(2021, 4, 16, 20, 0), duration: 120, slot: 101 }
+]);
---
+++ src/lib/ui/Employee.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { reservations } from '$lib/server/employee';
+	import { Button, ButtonSet, Tile, Grid, Row, Column } from 'carbon-components-svelte';
+	import { TrashCan16 } from 'carbon-icons-svelte';
+
+	function removeReservation(reservations, index) {
+		reservations;
+	}
+</script>
+
+<h2>Your reservations</h2>
+
+{#each $reservations as r, i}
+	<Tile>
+		<div>
+			{r.start} for {r.duration} minutes
+		</div>
+		<div>
+			<Button
+				kind="danger-tertiary"
+				iconDescription="Delete"
+				icon={TrashCan16}
+				on:click={() =>
+					reservations.update((r) => {
+						r.splice(i, 1);
+						return r;
+					})}
+			/>
+		</div>
+	</Tile>
+{:else}
+	<em>No reservations yet.</em>
+{/each}
