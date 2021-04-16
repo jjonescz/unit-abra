a118f7c7725b639cea41467a7f21a6228418c1c5
Jan Joneš <jjones@outlook.cz>
2021-04-16 15:43:27 +0200

    Use `date-fns` in employee endpoint

--- src/lib/server/employee.js
+++ src/lib/server/employee.js
@@ -1,8 +1,5 @@
-import dayjs from 'dayjs';
-import durationPlugin from 'dayjs/plugin/duration.js';
+import { differenceInMinutes } from 'date-fns';
 import fetch from 'node-fetch';
-
-dayjs.extend(durationPlugin);
 
 const endpoint = 'https://rezervace.flexibee.eu/c/rezervace8';
 
@@ -18,12 +15,12 @@ export async function getReservations(userName, authentication) {
     });
     const data = await response.json();
     return data.winstorm.udalost.map(u => {
-        const start = dayjs(u.zahajeni);
-        const end = dayjs(u.dokonceni);
+        const start = new Date(u.zahajeni);
+        const end = new Date(u.dokonceni);
         const slot = /code:(.*)/.exec(u.zakazka)[1];
         return {
             start: start,
-            duration: end.diff(start),
+            duration: differenceInMinutes(end, start),
             slot: slot
         };
     });
