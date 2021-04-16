f843ef66a2c6d3aeccd96de1e2995f9f5d3922cc
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-04-17 00:43:31 +0200

    Disallow deleting past reservations

--- src/lib/ui/calendar/DeleteReservation.svelte
+++ src/lib/ui/calendar/DeleteReservation.svelte
@@ -1,11 +1,12 @@
 <script>
 	import { deleteReservation } from '$lib/calendar';
 	import { Modal } from 'carbon-components-svelte';
-	import { format } from 'date-fns';
+	import { format, isAfter, addMinutes } from 'date-fns';
 	import { createEventDispatcher } from 'svelte';
 
 	export let open = false;
 	export let r = {};
+	let startFormated = '';
 
 	let authorization = 'Basic dGVhbTgudXppdmF0ZWwxOnRlYW04LUpXdGFr';
 
@@ -21,20 +22,23 @@
 		}
 		open = false;
 	}
+
+	$: canBeDeleted = isAfter(new Date(), addMinutes(r.start, r.duration));
 </script>
 
 <Modal
 	bind:open
 	danger
 	modalHeading="Delete reservation"
+	primaryButtonDisabled={canBeDeleted}
 	primaryButtonText="Delete"
 	secondaryButtonText="Cancel"
 	on:click:button--secondary={() => (open = false)}
 	on:open={() => {
-		r.start = format(r.start, 'yyyy-MM-dd H:mm');
+		startFormated = format(r.start, 'yyyy-MM-dd H:mm');
 	}}
 	on:close
 	on:submit={removeReservation}
 >
-	<p>Reservation on parking slot {r.slot} on {r.start} for {r.duration} minutes.</p>
+	<p>Reservation on parking slot {r.slot} on {startFormated} for {r.duration} minutes.</p>
 </Modal>
