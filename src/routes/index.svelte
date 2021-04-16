81455a114eb6c58508a0f8920b47e06ea067cc18
Jan Joneš <jjones@outlook.cz>
2021-04-16 09:08:09 +0200

    Use JavaScript

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,11 @@
-<script lang="ts">
+<script>
+	import { Button } from 'carbon-components-svelte';
+
+	let count = 0;
 </script>
 
 <svelte:head>
 	<title />
 </svelte:head>
+
+<Button on:click={() => count++}>Test {count}</Button>
